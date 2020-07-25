98058e6832e978afb097aa333fbb4249fb5f06f2
Demon000 <demonsingur@gmail.com>
2020-07-25 12:58:38 +0300

    msm: poweroff: fix build with download mode disabled
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -72,6 +72,7 @@ int download_mode = 0;
 int download_mode = 1;
 #endif
 static bool force_warm_reboot;
+static int in_panic;
 
 #ifdef CONFIG_QCOM_DLOAD_MODE
 #define EDL_MODE_PROP "qcom,msm-imem-emergency_download_mode"
@@ -80,7 +81,6 @@ static bool force_warm_reboot;
 #define KASLR_OFFSET_PROP "qcom,msm-imem-kaslr_offset"
 #endif
 
-static int in_panic;
 static struct kobject dload_kobj;
 static int dload_type = SCM_DLOAD_FULLDUMP;
 static void *dload_mode_addr;
