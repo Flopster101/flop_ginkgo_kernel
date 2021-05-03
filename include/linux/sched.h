8c6fa8dbb8c5a55101cf1eaaf70afce8d77924e2
LibXZR <xzr467706992@163.com>
2021-05-03 19:22:11 +0800

    fs: fuse: Implement fuse short circuit
    * This significantly improves i/o performance under /sdcard
    * From OnePlus 8T Oxygen OS 11.0.8.11.KB05AA and OnePlus 8 Oxygen OS 11.0.5.5.IN21AA and OnePlus 8 Pro Oxygen OS 11.0.5.5.IN11AA
    
    RealJohnGalt: make proper Kconfig, add back dependencies from OnePlus
    source onto our CAF tree.
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1382,6 +1382,10 @@ struct task_struct {
 	void				*security;
 #endif
 
+#ifdef CONFIG_FUSE_SHORTCIRCUIT
+	int fuse_boost;
+#endif
+
 	struct {
 		struct work_struct work;
 		atomic_t running;
@@ -1394,6 +1398,8 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
+	struct fuse_package *fpack;
+
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1403,6 +1409,12 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
+};
+
+struct fuse_package {
+	bool fuse_open_req;
+	struct file *filp;
+	char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -274,6 +274,7 @@ struct fuse_file_lock {
 #define FUSE_HANDLE_KILLPRIV	(1 << 19)
 #define FUSE_POSIX_ACL		(1 << 20)
 #define FUSE_ABORT_ERROR	(1 << 21)
+#define FUSE_SHORTCIRCUIT	(1 << 30)
 
 /**
  * CUSE INIT request/reply flags
