9af775ed1fbd3323e0003945f41d15e5c0c4494e
stic-server-open <1138705738@qq.com>
2022-05-10 19:48:17 +0800

    kernel: sysctl: Change swappiness max range to 200
    * By default only swappiness up to 100 is allowed,
    On some systems, the ZRAM space is often set to a large amount
    For example (MIUI), while MIUI uses its own memory management
    Take over the maximum swappiness, such as the camera process.
    So change this maximum exchange rate to 200, in the absence of
    Xiaomi's private changes rely on manual adjustments by users
    Alleviate the problem of ZRAM space waste [1].
    
    [1]: On the MIUI system, the ZRAM space is generally 75% of the
         physical memory, so MIUI uses its own mi_reclaim to take over. By
         default, the swappiness is 120. When calling the camera service, the
         swappiness will be adjusted to 190 to alleviate possible camera
         problems. Caton and kill background problems.
    
         On our CAFest kernel, the software process is often terminated by
         LMK before the ZRAM space is used, so this interface is opened for
         users to adjust from user space (root).
    
    * In general, the default value of 100 is the best choice. If you want to
      keep the most background in low memory, please try to set a higher
      ZRAM space (such as 80% of the physical space), and set theswappiness to 160 or 180.
    
    Change-Id: I4fbcb82b9e0c554cecd39cb4c6ad9770de49ef29
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -135,6 +135,7 @@ static unsigned long zero_ul;
 static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
+static int two_hundred = 200;
 static int one_thousand = 1000;
 
 /*dylanchang, 2019/4/30, add foreground task io opt*/
@@ -1688,7 +1689,7 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0444,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
-		.extra2		= &one_hundred,
+		.extra2		= &two_hundred,
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
