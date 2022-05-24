4c96b4bc163429c6dca9d2a4974eb434d9abf82f
Juhyung Park <qkrwngud825@gmail.com>
2022-05-24 21:54:28 +0900

    simple_lmk: expose simple_lmk_trigger()
    vmpressure won't be the only external caller.
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -463,6 +463,12 @@ void simple_lmk_mm_freed(struct mm_struct *mm)
 	read_unlock(&mm_free_lock);
 }
 
+void simple_lmk_trigger(void)
+{
+	if (!atomic_cmpxchg_acquire(&needs_reclaim, 0, 1))
+		wake_up(&oom_waitq);
+}
+
 static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 				    unsigned long pressure, void *data)
 {
@@ -470,7 +476,7 @@ static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 		atomic_set(&needs_reclaim, 1);
 		smp_mb__after_atomic();
 		if (waitqueue_active(&oom_waitq))
-			wake_up(&oom_waitq);
+			simple_lmk_trigger();
 	}
 
 	return NOTIFY_OK;
