111931fa0707a4fa5f2a31cf1d0ca42bf5e602c4
John Galt <johngaltfirstrun@gmail.com>
2023-05-01 12:41:29 -0400

    [DNM/TESTING]f2fs/segment.h: tune from testing
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- fs/f2fs/segment.h
+++ fs/f2fs/segment.h
@@ -612,9 +612,9 @@ static inline int utilization(struct f2fs_sb_info *sbi)
  * F2FS_IPU_NOCACHE - disable IPU bio cache.
  * F2FS_IPUT_DISABLE - disable IPU. (=default option in LFS mode)
  */
-#define DEF_MIN_IPU_UTIL	70
-#define DEF_MIN_FSYNC_BLOCKS	20
-#define DEF_MIN_HOT_BLOCKS	16
+#define DEF_MIN_IPU_UTIL	90
+#define DEF_MIN_FSYNC_BLOCKS	32
+#define DEF_MIN_HOT_BLOCKS	32
 
 #define SMALL_VOLUME_SEGMENTS	(16 * 512)	/* 16GB */
 
