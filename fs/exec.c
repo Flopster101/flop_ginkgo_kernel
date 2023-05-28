c798cadcc73288c79e1d1082b95a5e7f805e9b3c
Joel Gómez <nahuelgomez329@gmail.com>
2023-05-28 23:13:12 +0000

    fs: guard kernelsu-specific functions
    Fixes these linking errors:
    
    ld.lld: error: undefined symbol: ksu_handle_faccessat
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(sys_faccessat)
    
    ld.lld: error: undefined symbol: ksu_handle_vfs_read
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(vfs_read)
    
    ld.lld: error: undefined symbol: ksu_handle_stat
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(vfs_statx)
    
    ld.lld: error: undefined symbol: ksu_handle_execveat
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(do_execveat_common.llvm.1697053041754168844)
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1717,8 +1717,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+#endif
 
 /*
  * sys_execve() executes a new program.
@@ -1734,7 +1736,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+	#ifdef CONFIG_KSU
 	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
