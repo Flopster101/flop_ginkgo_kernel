485d53744aff1225e5367969f5ac7e5cc55016d2
Joel Gómez <nahuelgomez329@gmail.com>
2023-07-06 00:47:08 +0000

    fs / drivers: Implement new KernelSU hooks
    Following commit https://github.com/tiann/KernelSU/commit/bd8434f4f4f3e631f66d36ad8afdf7a4201c4851
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1704,8 +1704,11 @@ static int exec_binprm(struct linux_binprm *bprm)
 }
 
 #ifdef CONFIG_KSU
+extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
 #endif
 
 /*
@@ -1723,7 +1726,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 
 	#ifdef CONFIG_KSU
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
 	#endif
 
 	if (IS_ERR(filename))
