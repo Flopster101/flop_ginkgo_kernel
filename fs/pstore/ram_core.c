35cc91d131bc4b5dba6baf25f2cab73a63b4388c
Joel Gómez <thegame455min@gmail.com>
2023-09-15 02:32:41 -0300

    Revert "pstore/ram: Do not use stack VLA for parity workspace"
    This reverts commit cd5b210b249fd7cad0e057fab33c6a616d7fd8f7.

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -98,23 +98,24 @@ static void notrace persistent_ram_encode_rs8(struct persistent_ram_zone *prz,
 	uint8_t *data, size_t len, uint8_t *ecc)
 {
 	int i;
+	uint16_t par[prz->ecc_info.ecc_size];
 
 	/* Initialize the parity buffer */
-	memset(prz->ecc_info.par, 0,
-	       prz->ecc_info.ecc_size * sizeof(prz->ecc_info.par[0]));
-	encode_rs8(prz->rs_decoder, data, len, prz->ecc_info.par, 0);
+	memset(par, 0, sizeof(par));
+	encode_rs8(prz->rs_decoder, data, len, par, 0);
 	for (i = 0; i < prz->ecc_info.ecc_size; i++)
-		ecc[i] = prz->ecc_info.par[i];
+		ecc[i] = par[i];
 }
 
 static int persistent_ram_decode_rs8(struct persistent_ram_zone *prz,
 	void *data, size_t len, uint8_t *ecc)
 {
 	int i;
+	uint16_t par[prz->ecc_info.ecc_size];
 
 	for (i = 0; i < prz->ecc_info.ecc_size; i++)
-		prz->ecc_info.par[i] = ecc[i];
-	return decode_rs8(prz->rs_decoder, data, prz->ecc_info.par, len,
+		par[i] = ecc[i];
+	return decode_rs8(prz->rs_decoder, data, par, len,
 				NULL, 0, NULL, 0, NULL);
 }
 
@@ -225,15 +226,6 @@ static int persistent_ram_init_ecc(struct persistent_ram_zone *prz,
 	if (prz->rs_decoder == NULL) {
 		pr_info("init_rs failed\n");
 		return -EINVAL;
-	}
-
-	/* allocate workspace instead of using stack VLA */
-	prz->ecc_info.par = kmalloc_array(prz->ecc_info.ecc_size,
-					  sizeof(*prz->ecc_info.par),
-					  GFP_KERNEL);
-	if (!prz->ecc_info.par) {
-		pr_err("cannot allocate ECC parity workspace\n");
-		return -ENOMEM;
 	}
 
 	prz->corrected_bytes = 0;
@@ -542,13 +534,6 @@ void persistent_ram_free(struct persistent_ram_zone *prz)
 		}
 		prz->vaddr = NULL;
 	}
-	if (prz->rs_decoder) {
-		free_rs(prz->rs_decoder);
-		prz->rs_decoder = NULL;
-	}
-	kfree(prz->ecc_info.par);
-	prz->ecc_info.par = NULL;
-
 	persistent_ram_free_old(prz);
 	kfree(prz);
 }
