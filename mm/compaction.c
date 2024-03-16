f17c6cb14ee7c0ceedecb57b3d7aff6fa3f555a6
Joel Gómez <nahuelgomez329@gmail.com>
2024-03-16 09:57:10 -0300

    Revert "mm: Run ZRAM compaction on automatic compaction"
    This reverts commit efbfaf6376f10bafdade9b09cd4d62e443b64c0c.

--- mm/compaction.c
+++ mm/compaction.c
@@ -1923,8 +1923,6 @@ static void compact_nodes(void)
 		compact_node(nid);
 }
 
-void zram_compact(void);
-
 static void do_compaction(struct work_struct *work)
 {
 	/* Return early if the screen is on */
@@ -1935,9 +1933,6 @@ static void do_compaction(struct work_struct *work)
 
 	/* Do full compaction */
 	compact_nodes();
-
-	/* Do ZRAM compaction */
-	zram_compact();
 
 	/* Force compaction timeout */
 	compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
