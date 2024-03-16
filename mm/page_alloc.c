fa6763562a0feb76759e83eefae7c0a5ae66e37a
Joel Gómez <nahuelgomez329@gmail.com>
2024-03-16 15:16:21 -0300

    Revert "[REAPPLY] mm: Boost when memory pressure becomes high"
    This reverts commit 2f1334a15c271a8ee11449fa0123cb841879ae41.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -71,7 +71,6 @@
 #include <linux/nmi.h>
 #include <linux/khugepaged.h>
 #include <linux/psi.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
@@ -4225,10 +4224,6 @@ retry:
 	/* Ensure kswapd doesn't accidentally go to sleep as long as we loop */
 	if (gfp_mask & __GFP_KSWAPD_RECLAIM)
 		wake_all_kswapds(order, gfp_mask, ac);
-
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	reserve_flags = __gfp_pfmemalloc_flags(gfp_mask);
 	if (reserve_flags)
