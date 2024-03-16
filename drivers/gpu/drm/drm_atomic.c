061901760b896f443e94995b4c2c80bdfb073a2b
Joel Gómez <nahuelgomez329@gmail.com>
2024-03-16 15:14:11 -0300

    Revert "drivers: cpufreq: Bring back CPU Input Boost"
    This reverts commit b76ebed48240a6b7bd57626411669ea6a9773023.

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -30,7 +30,6 @@
 #include <drm/drm_atomic.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
-#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
@@ -2252,7 +2251,6 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		cpu_input_boost_kick();
 
 	drm_modeset_acquire_init(&ctx, 0);
 
