1ee95be727d1821b152c57c99af97bd5cd7c500a
Joel Gómez <nahuelgomez329@gmail.com>
2024-03-16 15:16:07 -0300

    Revert "[REAPPLY] kernel: Boost CPU & DDR bus for a short amount of time when zygote forks"
    This reverts commit 63c945e9428943d4c6d22e415ff1c5f7e038b879.

--- kernel/fork.c
+++ kernel/fork.c
@@ -94,7 +94,6 @@
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
-#include <linux/cpu_input_boost.h>
 
 #include <linux/devfreq_boost.h>
 #include <asm/pgtable.h>
@@ -2235,7 +2234,6 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current))
-		cpu_input_boost_kick_max(50);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 
 	/*
